28ad8b15635bb81f22d5b6da68f79d34e3a5f2da
sumitrsch <104359443+sumitrsch@users.noreply.github.com>
2025-02-25 14:39:05 +0530

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {	
+	public int addition(int num1, int num2)
+	{
+		return (num1+num2);
+	}
+	public int substraction(int num1, int num2)
+	{
+		return (num1-num2);
+	}
+}
---
+++ src/MainCalculator.java
@@ -0,0 +1,9 @@
+public class MainCalculator {
+	public static void main(String[] args) {		
+		Calculator calc = new Calculator();
+		int sum = calc.addition(10, 20);
+		System.out.println("Addition is "+sum);
+		int sub = calc.substraction(20, 10);
+		System.out.println("Substraction is "+sub);
+	}
+}
